70f68d81d5ced0f0e8a83b5bcd81858600c745a3
idecay <greenwood.cm88@gmail.com>
2020-02-10 03:26:47 -0500

    rearrange nav links

--- client/src/App.js
+++ client/src/App.js
@@ -6,20 +6,30 @@ import ChoreList from "./components/ChoresList";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import ChoresList from "./components/ChoresList";
 import Home from "./components/Home";
+import SingleMember from "./components/SingleMember";
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <Router>
-          <Link to="/createmember">Create Member</Link>
-          <Link to="/createchore">Create Chore</Link>
-          <Link to="/home">Home</Link>
+          <nav>
+            <Link to="/home"> Home</Link> \\
+            <Link to="/createmember"> Family Members</Link> \\
+            <Link to="/createchore"> Create Chore</Link>
+          </nav>
+
+          {/* <SingleMember /> */}
           <Switch>
             <Route exact path="/choreslist" component={ChoresList} />
             <Route exact path="/createmember" component={CreateMember} />
             <Route exact path="/createchore" component={CreateChore} />
             <Route exact path="/home" component={Home} />
+            <Route
+              exact
+              path="/familymember/:memberId"
+              component={SingleMember}
+            ></Route>
           </Switch>
         </Router>
       </div>
