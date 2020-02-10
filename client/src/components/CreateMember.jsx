971f4cae9b53848e05c2662202ac13d30f39c4db
idecay <greenwood.cm88@gmail.com>
2020-02-10 03:28:23 -0500

    create button for user to toggle create and list pages

--- client/src/components/CreateMember.jsx
+++ client/src/components/CreateMember.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 export default class CreateMember extends Component {
   state = {
@@ -9,7 +10,8 @@ export default class CreateMember extends Component {
       full_name: "",
       is_parent: Boolean,
       profile_image: ""
-    }
+    },
+    createMember: false
   };
 
   componentDidMount = async () => {
@@ -72,41 +74,59 @@ export default class CreateMember extends Component {
     this.setState({ cancelRedirect: true });
   };
 
+  toggleCreateMember = () => {
+    const toggle = !this.state.createMember;
+    this.setState({ createMember: toggle });
+  };
+
   render() {
+    const allFamilyMembers = this.state.familyMembers.map(member => {
+      return (
+        <Link to={`/familymember/${member.id}`}>
+          <div>{member.full_name}</div>
+        </Link>
+      );
+    });
     return (
       <div>
-        <h1>Create Family Member</h1>
-        <form onSubmit={this.onSubmit}>
-          <label>Full Name: </label>
-          <input
-            type="text"
-            placeholder="enter full name here"
-            name="full_name"
-            onChange={this.onChange}
-            value={this.state.newMember.full_name}
-          />
-          <label>Profile Picture: </label>
-          <input
-            type="text"
-            placeholder="input image URL"
-            name="profile_image"
-            onChange={this.onChange}
-            value={this.state.newMember.profile_image}
-          />
-          <label>Is this person a parent? </label>
-          <input
-            type="checkbox"
-            name="is_parent"
-            onChange={this.handleInputChange}
-            checked={this.state.newMember.is_parent}
-          />
-          {/* TODO: figure out why the submit button wont work with box is checked */}
-          <input type="submit" value="Create" />
-        </form>
-
-        {this.state.familyMembers.map(member => (
-          <h3>{member.full_name} </h3>
-        ))}
+        <button onClick={this.toggleCreateMember}>New Family Member</button>
+        {this.state.createMember === true ? (
+          <div className="createMemberForm">
+            <h1>Create Family Member</h1>
+            <form onSubmit={this.onSubmit}>
+              <label>Full Name: </label>
+              <input
+                type="text"
+                placeholder="enter full name here"
+                name="full_name"
+                onChange={this.onChange}
+                value={this.state.newMember.full_name}
+              />
+              <label>Profile Picture: </label>
+              <input
+                type="text"
+                placeholder="input image URL"
+                name="profile_image"
+                onChange={this.onChange}
+                value={this.state.newMember.profile_image}
+              />
+              <label>Is this person a parent? </label>
+              <input
+                type="checkbox"
+                name="is_parent"
+                onChange={this.handleInputChange}
+                checked={this.state.newMember.is_parent}
+              />
+              {/* TODO: figure out why the submit button wont work with box is checked */}
+              <input type="submit" value="Create" />
+            </form>
+          </div>
+        ) : (
+          <div className="membersList">
+            <h1>All Family Members</h1>
+            {allFamilyMembers}
+          </div>
+        )}
       </div>
     );
   }
